b83ab9fb307a10e624a95bfc5ed7869375849998
MBGomes76 <mateobatistagomes@gmail.com>
2023-02-04 14:39:29 +0100

    last commit

--- script.js
+++ script.js
@@ -3,58 +3,93 @@ window.onload = init;
 const QUOTE_URL = "https://api.quotable.io";
 
 let main;
-let divQuote;
+let divQuotes;
+let divBtnAuthor;
+let paraName;
+let paraTheme;
 
 async function init() {
-	main = document.querySelector("main");
-	divQuote = document.querySelector("main .quote");
+	main         = document.querySelector("main");
+	divQuotes    = document.querySelector("main .list-quote");
+	divBtnAuthor = document.querySelector("main .btn-author");
+	paraName     = document.querySelector("main .btn-author .name");
+	paraTheme    = document.querySelector("main .btn-author .theme");
 
+	randomQuoteView();
+}
+
+async function randomQuoteView() {
 	const randomQuote = getRandomQuote();
 	randomQuote.then((json) => {
-		console.log("json", json);
+		// On réinitialise la div des quote
+		divQuotes.innerHTML = "";
+		
 		// Ajout du paragraphe à la div
-		divQuote.innerHTML = `<p>${json.content}</p>`;
+		divQuotes.innerHTML = `
+			<div class="quote">
+				<div class="rectangle"></div>
+				<p>“${json.content}”</p>
+			</div>
+		`;
 
-		// Ajout du bouton pour accéder aux autres citations de l'auteur
-		// Création du paragraphe contenant le nom de l'auteur
-		const pName = document.createElement("p");
-		pName.classList.add("name");
-		pName.innerText = json.author;
+		// Ajout des infos sur l'auteur
+		paraName.innerText  = json.author;
+		paraTheme.innerText = json.tags[0];
 
-		// Création du paragraphe contenant le theme de la citation
-		const pTheme = document.createElement("p");
-		pTheme.classList.add("theme");
-		pTheme.innerText = json.tags[0];
-
-		// Création de la div author et ajouts des paragraphes
-		const divAuthor = document.createElement("div");
-		divAuthor.classList.add("author");
-		divAuthor.append(pName);
-		divAuthor.append(pTheme);
-
-		// Création de la flèche du bouton
-		const divArrow = document.createElement("div");
-		divArrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="white" height="48" width="48"><path d="m28.05 35.9-2.15-2.1 8.4-8.4H8v-3h26.3l-8.45-8.45 2.15-2.1L40.05 23.9Z"/></svg>`;
-		main.append(divArrow);
-
-		// Création de la div btn-author
-		const divBtnAuthor = document.createElement("div");
-		divBtnAuthor.classList.add("btn-author");
-		divBtnAuthor.append(divAuthor);
-		// divBtnAuthor.append(divBtnAuthor);
-
-		// On ajoute le tout au main
-		main.append(divBtnAuthor);
+		// On enlève la classe .hide
+		divBtnAuthor.classList.remove("hide");
 	});
 }
 
+async function authorQuotesView() {
+
+	const authorQuotes = getQuotesByAuthor();
+	authorQuotes.then((json) => {
+
+		console.log("json", json);
+		// On réinitialise la div des quote
+		divQuotes.innerHTML = "";
+
+		// On affiche l'auteur
+		const h2Author = document.createElement("h2");
+		h2Author.innerText = json.results[0].author;
+		divQuotes.append(h2Author);
+
+		// On boucle sur tous les résultats trouvés et on les affiches les uns en dessous des autres
+		json.results.forEach((result) => {
+
+			console.log("result", result);
+			// On crée la div qui va englober le tout
+			const divQuote = document.createElement("div");
+			divQuote.classList.add("quote");
+
+			// On crée la div qui va contenir le rectangle
+			const divRectangle = document.createElement("div");
+			divRectangle.classList.add("rectangle");
+
+			// On crée le paragraphe qui va contenir la quote
+			const paraQuote = document.createElement("p");
+			paraQuote.innerText = `“${result.content}”`;
+			divQuote.append(divRectangle);
+			divQuote.append(paraQuote);
+
+			// On ajoute la div quote à la liste des quotes
+			divQuotes.append(divQuote);
+
+		});
+
+		divBtnAuthor.classList.add("hide");
+	});
+}
 
 async function getRandomQuote() {
+
 	const responseRandomQuote = await fetch(`${QUOTE_URL}/random`);
 	return responseRandomQuote.json();
 }
 
-async function getQuotesByAuthor(author) {
-	const responseRandomQuote = await fetch(`${QUOTE_URL}/quotes&author=${author}`);
+async function getQuotesByAuthor() {
+
+	const responseRandomQuote = await fetch(`${QUOTE_URL}/quotes?author=${paraName.textContent}`);
 	return responseRandomQuote.json();
 }
